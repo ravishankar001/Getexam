d7065880b97fca4d4edae88b60655e9ca833dc1f
ravishankar001 <er.ravi201189@gmail.com>
2016-12-02 03:22:24 -0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,91 @@
+<?php
+include("header.php");
+?>
+<div class="container">
+  <hr>
+  <div class="row fonfamily">
+    <div class="col-sm-6 col-md-6">
+      <div>
+        <h1>About Us</h1>
+        <p><h4>Hello friends we are a team who are providing all the information related to every sector like <b>Government Exams,General knowledge,Current Affairs,Apptitude,Reasoning etc.</b>This website is all in one website where you will get not only the information but also tutorials and online exams.In this website we will provide following things to enhance your knowlwdge which are... </h4></p>
+        <ul>
+          <li><h4>Latest update of Government Exams,Their Admit card and Result.</h4></li>
+          <li><h4>Questions of General knowledge</h4></li>
+          <li><h4>Questions of current affairs.</h4></li>
+          <li><h4>Online tests</h4></li>
+		  <li><h4>Vedio tutorials</h4></li>
+        </ul>
+		<p><h4>We have a clear vision to become multi-skilled and multi-dimensional Education service providers with a focus on high end strategic solutions along with the ultimate aim to evolve and become a multi-dimensional service providers. We have all the capabilities to provide integrated online educational solutions.
+       Our focus is not only in education sector specific as we strongly feel that only the expertise of our team matters in everything that we do.</h4></p><p> <h4>We are committed to create excellent interactive experiences while designing, executing and managing online strategies for your carrier and in turn improving your knowledge.
+        Our multifunctional team is looking for a connection and interaction with your organization therefore, forming a long term business relationship and becoming a partner with you on a mutual growth path. </h4></p>
+       
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-6">
+     <a href="#"><img src="img/apprentice.jpg" alt=""></a>
+      <h3>Book </h3>
+      <a href="#"><img src="img/book-1.jpg" alt=""></a> </div>
+  </div>
+  <!--Start second row of columns-->
+  <div class="row">
+    <div class="col-sm-4 col-md-4">
+      <h1>Our Strength</h1>
+      <p><h4>We consists of every individual possessing best talent & having sound educational background with having sufficient relevant experience in their respective field. We are a team driven and result conferring professionals with a clear focus on students need. We are evolving a culture with best values which is necessary for our existence and achievement of long term goal. </h4></p>
+    </div>
+    <div class="col-sm-4 col-md-4">
+      <h1>Our Goal</h1>
+      <p><h4>Our goal is very clear to develop and enhance the knowledge of students who are preparing for government exams.They are always needs the best knowledge regarding different feild like <b>Apptitude,Reasoning,GK,Current affairs,Government exam latest vacancy etc.</b>
+	  Here we always try to provide latest update regarding to subjects and vacancies.</h4></p>
+    </div>
+    <div class="col-sm-4 col-md-4">
+      <h1>Our Achivements</h1>
+      <p><h4>We are on the way to achiving our goal because we are providing best and most important tutorials and online tests to improve your speed and enhance your knowledge.</h4></p>
+    </div>
+  </div>
+  <hr>
+  <!--footer class="row">
+    <div>
+      <div class="span4">
+        <div class="is-padded">
+          <nav>
+            <h2>Navigation</h2>
+            <hr>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">About</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="span4">
+        <div class="is-padded">
+          <h2>Facebook</h2>
+          <hr>
+          <a href="#">@AwfulMedia</a>
+          <p>This is what my tweet looks like on this website. Tweet tweet. Twitter Twatter.</p>
+          <em>3 days ago</em><br>
+          <br>
+          <a href="#">@AwfulMedia</a>
+          <p>This is what my tweet looks like on this website. Tweet tweet. Twitter Twatter.</p>
+          <em>3 days ago</em> </div>
+      </div>
+      <div class="span4">
+        <div class="is-padded">
+          <h2>Details</h2>
+          <blockquote>Respond is a fully responsive website template utilizing Twitter's Bootstrap framework. What does that mean? First of all, the website will respond according to the size of the viewers browser window. Go on, try it out. Resize your window. On top of that, the framework offers loads of ready-to-go features. Check it out with the button below. <br>
+            <br>
+            <em>- AwfulMedia.com</em> </blockquote>
+        </div>
+      </div>
+    </div>
+    <p> &copy;2012 Your Company.<br>
+      Design by <a href="http://www.awfulmedia.com">AwfulMedia.com</a> </p>
+  </footer-->
+</div>
+<!-- /container -->
+<?php
+include("footer.php");
+?>
