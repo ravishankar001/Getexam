9f17517d4f16846f34f2adb8cd3b69f9f3f6fa85
ravishankar001 <er.ravi201189@gmail.com>
2016-12-02 03:33:00 -0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', ' ');
-   define('DB_DATABASE', 'getex2am');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   if($db)
-	echo "error";
-?>
